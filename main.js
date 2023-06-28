9251b8e587c61d8d338b8f6691f4a754a0015ca3
valgor007 <rafaupup@gmail.com>
2023-06-27 19:06:23 -0600

    Working with the buttons, Added black, and random color, also resetBoard

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
+let color = 'black';
+
 document.addEventListener('DOMContentLoaded', function(){
 
-    createBoard(32);
-    console.log('hi');
+    createBoard(16);
+
+    let btn_popup = document.querySelector("#popup");
+    btn_popup.addEventListener('click', function(){
+        let size = getSize();
+        createBoard(size);
+    });
 })
 
 function createBoard(size){
@@ -13,7 +20,38 @@ function createBoard(size){
 
     for(let i= 0; i < numDivs; i++){
         let div = document.createElement('div');
-        div.style.backgroundColor = 'yellow';
+        div.addEventListener('mouseover', colorDiv )
         board.insertAdjacentElement("beforeEnd", div)
     }
+}
+
+
+function getSize(){
+    let input = prompt("What will be the size of the board?");
+    let message = document.querySelector('#message')
+    if(input == ""){
+message.innerHTML = "Please provide a number"
+    }else if(input <= 0 || input > 100){
+        message.innerHTML = "Provide a number between 1 and 100"
+    }else{
+        message.innerHTML = "Now you can play"
+        return input;
+    }
+}
+
+function colorDiv(){
+    if(color == 'random'){
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+    }else{
+        this.style.backgroundColor = 'black'
+    }
+}
+
+function setColor(colorChoice){
+     color = colorChoice
+}
+
+function resetBoard(){
+    let divs = document.querySelectorAll('div');
+    divs.forEach(element => element.style.backgroundColor = 'white');
 }
